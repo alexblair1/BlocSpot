a9102700552a37c9636020a0d134fd2368aedd6b
Alex Blair <alexblair190@gmail.com>
2015-07-15 20:42:26 -0600

    magical record

---
+++ BlocSpot/POI.h
@@ -0,0 +1,17 @@
+//
+//  POI.h
+//  BlocSpot
+//
+//  Created by Stephen Blair on 7/15/15.
+//  Copyright (c) 2015 blairgraphix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface POI : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+
+@end
---
+++ BlocSpot/POI.m
@@ -0,0 +1,16 @@
+//
+//  POI.m
+//  BlocSpot
+//
+//  Created by Stephen Blair on 7/15/15.
+//  Copyright (c) 2015 blairgraphix. All rights reserved.
+//
+
+#import "POI.h"
+
+
+@implementation POI
+
+@dynamic name;
+
+@end
